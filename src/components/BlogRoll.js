a5cc70ed76c8f9d5e06ea0ac9575839ead9cee26
Vassilis <vas.terzo@gmail.com>
2019-11-21 13:13:02 +0200

    update blogroll

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,74 +3,72 @@ import PropTypes from 'prop-types';
 import { Link, graphql, StaticQuery } from 'gatsby';
 import PreviewCompatibleImage from './PreviewCompatibleImage';
 
-class BlogRoll extends React.Component {
-    render() {
-        const { data } = this.props;
-        const { edges: posts } = data.allMarkdownRemark;
+function BlogRoll(props) {
+  const { data } = props;
+  const { edges: posts } = data.allMarkdownRemark;
 
-        return (
-            <>
-                {posts &&
-          posts.map(({ node: post }) => (
-              <div
-                  style={{
-                      marginBottom: '2rem',
-                  }}
-              >
-                  <article
-                      key={post.id}
-                      className={`blog-list-item tile is-child notification ${
-                          post.frontmatter.featuredpost ? 'is-featured' : ''
-                      }`}
+  return (
+    <>
+      {posts &&
+        posts.map(({ node: post }) => (
+          <div
+            style={{
+              marginBottom: '2rem',
+            }}
+          >
+            <article
+              key={post.id}
+              className={`blog-list-item tile is-child notification ${
+                post.frontmatter.featuredpost ? 'is-featured' : ''
+              }`}
+            >
+              <header>
+                {post.frontmatter.featuredimage ? (
+                  <div className="featured-thumbnail">
+                    <PreviewCompatibleImage
+                      imageInfo={{
+                        image: post.frontmatter.featuredimage,
+                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                      }}
+                    />
+                  </div>
+                ) : null}
+                <p className="post-meta">
+                  <Link
+                    className="title has-text-dark is-size-4"
+                    to={post.fields.slug}
                   >
-                      <header>
-                          {post.frontmatter.featuredimage ? (
-                              <div className="featured-thumbnail">
-                                  <PreviewCompatibleImage
-                                      imageInfo={{
-                                          image: post.frontmatter.featuredimage,
-                                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                      }}
-                                  />
-                              </div>
-                          ) : null}
-                          <p className="post-meta">
-                              <Link
-                                  className="title has-text-dark is-size-4"
-                                  to={post.fields.slug}
-                              >
-                                  {post.frontmatter.title}
-                              </Link>
-                              <small className="is-block">{post.frontmatter.date}</small>
-                          </p>
-                      </header>
-                      <p>
-                          {post.excerpt}
-                          <br />
-                          <br />
-                          <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                          </Link>
-                      </p>
-                  </article>
-              </div>
-          ))}
-            </>
-        );
-    }
+                    {post.frontmatter.title}
+                  </Link>
+                  <small className="is-block">{post.frontmatter.date}</small>
+                </p>
+              </header>
+              <p>
+                {post.excerpt}
+                <br />
+                <br />
+                <Link className="button is-small" to={post.fields.slug}>
+                  Keep Reading →
+                </Link>
+              </p>
+            </article>
+          </div>
+        ))}
+    </>
+  );
 }
 
 BlogRoll.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            edges: PropTypes.array,
-        }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
     }),
+  }),
 };
 
 export default () => (
-    <StaticQuery
-        query={graphql`
+  <StaticQuery
+    query={graphql`
       query BlogRollQuery {
         allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
@@ -101,6 +99,6 @@ export default () => (
         }
       }
     `}
-        render={(data, count) => <BlogRoll data={data} count={count} />}
-    />
+    render={(data, count) => <BlogRoll data={data} count={count} />}
+  />
 );
